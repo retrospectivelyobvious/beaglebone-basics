e6395683045e71a464da9f58d76924d616db0a2f
Bob Sherbert <bob.sherbert@gmail.com>
2012-09-23 15:18:00 -0400

    Getting Started with the BeagleBone
    Content to accompany article.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-beaglebone-basics
+BeagleBone Basics
 =================
+
+[Tutorials](http://retrospectivelyobvious.org/beaglebone-basics/)
+
+[GitHub](https://github.com/retrospectivelyobvious/beaglebone-basics)
+
+Contact
+-------
+Bob Sherbert <bob@retrospectivelyobvious.org>
+
+Legal
+-----
+BeagleBone Basics is copyright Bob Sherbert 2012 and is released under the
+GPLv3 (see COPYING). It is free to use, abuse, modify, etc in accordance with
+the licence terms. If you find the software to be useful we request that you
+provide credit to the authors by linking to the original tutorial pages. If you
+find these licence terms too restrictive or wish to profit from this work in
+some way, you may contact the authors to arrange commercial licencing.
+
+Intro
+----
+BeagleBone Basics is a collection of small tools, scripts, and utilities to
+help folks get up and running with the BeagleBone platform. The goal is to
+eliminate frustration, elucidate some of the more confusing aspects of the
+setup/usage process, and overall serve to lower barriers to entry with the
+platform.
+
---
+++ minicom/install-minicom-config.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#BeagleBone Basics
+#Copyright (C) 2012 Robert M. Sherbert
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#If by some strange twist you believe that this software would have value for a
+#commercial project but are unwilling to be bound by the strictures of the GPL,
+#you may contact the author with inquiries for alternative licence terms.
+
+set -e  #quit on error
+
+MINICOMDIR=/etc/minicom
+
+if [ -d $MINICOMDIR ]; then
+    echo "Copying Config file"
+    sudo cp minirc.beagleserial $MINICOMDIR/
+    echo "Success"
+else
+    echo "Can't figure out where to put the config files."
+    echo "You'll have to install them manually."
+    exit 1
+fi
